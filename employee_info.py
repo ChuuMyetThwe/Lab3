18661144bff0bfeb34dd7af7d8ce13cb81857dbb
Chuu Myet Thwe <CHUUMYET.22@ichat.sp.edu.sg>
2023-11-05 03:06:57 +0800

    Exercise 5

--- employee_info.py
+++ employee_info.py
@@ -22,15 +22,22 @@ def calculate_average_salary():
     total = 0
     average = 0
 
-    #add your implementation to calculate here
+    for item in employee_data:
+        total = total + item['salary']
 
+    average = total/len(employee_data)
 
     return average
 
 def get_employees_by_dept(department):
     result = []
 
-    # Add your implementation from here
+    for item in employee_data:
+        if item['department'] == department:
+            result.append(item)
+
+    return result
+
 
 
     return result
@@ -67,7 +74,7 @@ def display_main_menu():
 
     elif option == '2':
         average_salary = calculate_average_salary()
-        print("Average salary = " + str(average_salary))
+        print("Average salary = " + "{:.2f}".format(average_salary))
 
     elif option == '3':
         age_lower_limit = input("age (Lower Limit) = ")
